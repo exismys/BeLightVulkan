3f78ce3945791a0859ee58d844e50bc3113158a4
Ritesh Patel <riteshzeronity@gmail.com>
2026-09-14 21:25:27 +0530

    Initial vulkan setup

---
+++ src/main.cpp
@@ -0,0 +1,134 @@
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+const std::vector<char const*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication {
+    public:
+        void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+    private:
+        GLFWwindow *window = nullptr;
+        vk::raii::Context  context;
+        vk::raii::Instance instance = nullptr;
+
+        void initWindow() {
+            glfwInit();
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        }
+
+        void initVulkan() {
+            createInstance();
+        }
+
+        void mainLoop() {
+            while (!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
+            }
+        }
+
+        void cleanup() {
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        }
+
+        void createInstance() {
+            std::vector<char const*> requiredLayers;
+            if (enableValidationLayers) {
+                requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+            }
+
+            auto layerProperties = context.enumerateInstanceLayerProperties();
+            auto unsupportedLayerIt = std::ranges::find_if(requiredLayers, [&layerProperties](auto const &requiredLayer) {
+                return std::ranges::none_of(layerProperties, [requiredLayer](auto const &layerProperty) {
+                    return strcmp(layerProperty.layerName, requiredLayer) == 0;
+                });
+            });
+
+            if (unsupportedLayerIt != requiredLayers.end()) {
+                throw std::runtime_error("Required layer not supported: " + std::string(*unsupportedLayerIt));
+            }
+
+            auto requiredExtensions = getRequiredInstanceExtensions();
+
+            auto extensionProperties = context.enumerateInstanceExtensionProperties();
+            auto unsupportedPropertyIt = std::ranges::find_if(requiredExtensions, [&extensionProperties](auto const &requiredExtension) {
+                return std::ranges::none_of(extensionProperties, [requiredExtension](auto const &extensionProperty) {
+                    return strcmp(extensionProperty.extensionName, requiredExtension) == 0; 
+                });
+            });
+
+            if (unsupportedPropertyIt != requiredExtensions.end()) {
+                throw std::runtime_error("Required extension not supported: " + std::string(*unsupportedPropertyIt));
+            }
+
+            constexpr vk::ApplicationInfo appInfo{
+                .pApplicationName   = "Hello Triangle",
+                .applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+                .pEngineName        = "No Engine",
+                .engineVersion      = VK_MAKE_VERSION( 1, 0, 0 ),
+                .apiVersion         = vk::ApiVersion14
+            };
+
+            vk::InstanceCreateInfo createInfo{
+                .pApplicationInfo        = &appInfo,
+                .enabledLayerCount       = static_cast<uint32_t>(requiredLayers.size()),
+                .ppEnabledLayerNames     = requiredLayers.data(),
+                .enabledExtensionCount   = static_cast<uint32_t>(requiredExtensions.size()),
+                .ppEnabledExtensionNames = requiredExtensions.data()
+            };
+
+            instance = vk::raii::Instance(context, createInfo);
+        }
+
+        std::vector<const char*> getRequiredInstanceExtensions() {
+            uint32_t glfwExtensionCount = 0;
+            auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+            return extensions;
+        }
+};
+
+int main() {
+    try {
+        HelloTriangleApplication app;
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
