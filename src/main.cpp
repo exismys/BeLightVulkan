21f12806f01307e94b20aa731f9638031a5f0508
Ritesh Patel <riteshzeronity@gmail.com>
2026-09-18 03:52:56 +0530

    Create graphics pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,8 @@ class HelloTriangleApplication {
         vk::raii::SwapchainKHR swapChain = nullptr;
         std::vector<vk::Image> swapChainImages;
         std::vector<vk::raii::ImageView> swapChainImageViews;
+        vk::raii::PipelineLayout pipelineLayout = nullptr;
+        vk::raii::Pipeline graphicsPipeline = nullptr;
 
 	    std::vector<const char *> requiredDeviceExtension = {vk::KHRSwapchainExtensionName};
 
@@ -367,6 +369,71 @@ class HelloTriangleApplication {
             vk::PipelineShaderStageCreateInfo vertShaderStageInfo{.stage = vk::ShaderStageFlagBits::eVertex, .module = shaderModule, .pName = "vertMain"};
             vk::PipelineShaderStageCreateInfo fragShaderStageInfo{.stage = vk::ShaderStageFlagBits::eFragment, .module = shaderModule, .pName = "fragMain"};
             vk::PipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+            vk::PipelineVertexInputStateCreateInfo vertexInputInfo;
+            
+            vk::PipelineInputAssemblyStateCreateInfo inputAssembly{.topology = vk::PrimitiveTopology::eTriangleList};
+            vk::PipelineViewportStateCreateInfo      viewportState{.viewportCount = 1, .scissorCount = 1};
+
+            vk::PipelineRasterizationStateCreateInfo rasterizer{
+                .depthClampEnable        = vk::False,
+                .rasterizerDiscardEnable = vk::False,
+                .polygonMode             = vk::PolygonMode::eFill,
+                .cullMode                = vk::CullModeFlagBits::eBack,
+                .frontFace               = vk::FrontFace::eClockwise,
+                .depthBiasEnable         = vk::False,
+                .lineWidth               = 1.0f
+            };
+
+            vk::PipelineMultisampleStateCreateInfo multisampling{.rasterizationSamples = vk::SampleCountFlagBits::e1, .sampleShadingEnable = vk::False};
+
+            vk::PipelineColorBlendAttachmentState colorBlendAttachment{
+                .blendEnable    = vk::False,
+                .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
+            };
+
+            vk::PipelineColorBlendStateCreateInfo colorBlending{
+                .logicOpEnable = vk::False, 
+                .logicOp = vk::LogicOp::eCopy, 
+                .attachmentCount = 1, 
+                .pAttachments = &colorBlendAttachment
+            };
+
+            std::vector<vk::DynamicState> dynamicStates = {vk::DynamicState::eViewport, vk::DynamicState::eScissor};
+            vk::PipelineDynamicStateCreateInfo dynamicState{
+                .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()), 
+                .pDynamicStates = dynamicStates.data()
+            };
+
+            vk::PipelineLayoutCreateInfo pipelineLayoutInfo{.setLayoutCount = 0, .pushConstantRangeCount = 0};
+            pipelineLayout = vk::raii::PipelineLayout(device, pipelineLayoutInfo);
+
+            vk::PipelineRenderingCreateInfo pipelineRenderingCreateInfo{ 
+                .colorAttachmentCount = 1, 
+                .pColorAttachmentFormats = &swapChainSurfaceFormat.format 
+            };
+
+            vk::StructureChain<vk::GraphicsPipelineCreateInfo, vk::PipelineRenderingCreateInfo> pipelineCreateInfoChain = {
+                {
+                    .stageCount          = 2,
+                    .pStages             = shaderStages,
+                    .pVertexInputState   = &vertexInputInfo,
+                    .pInputAssemblyState = &inputAssembly,
+                    .pViewportState      = &viewportState,
+                    .pRasterizationState = &rasterizer,
+                    .pMultisampleState   = &multisampling,
+                    .pColorBlendState    = &colorBlending,
+                    .pDynamicState       = &dynamicState,
+                    .layout              = pipelineLayout,
+                    .renderPass          = nullptr
+                },
+                {
+                    .colorAttachmentCount = 1, 
+                    .pColorAttachmentFormats = &swapChainSurfaceFormat.format
+                }
+            };
+
+            graphicsPipeline = vk::raii::Pipeline(device, nullptr, pipelineCreateInfoChain.get<vk::GraphicsPipelineCreateInfo>());
         }
 
         [[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char> &code) const {
