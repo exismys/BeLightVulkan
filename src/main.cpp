743b10e8a2f89b5ed98b6d61aa050d3883ae4030
Ritesh Patel <riteshzeronity@gmail.com>
2026-09-17 19:11:30 +0530

    Create shader module

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ import vulkan_hpp;
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <fstream>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -361,8 +362,31 @@ class HelloTriangleApplication {
         }
 
         void createGraphicsPipeline() {
+            vk::raii::ShaderModule shaderModule = createShaderModule(readFile("shaders/slang.spv"));
 
+            vk::PipelineShaderStageCreateInfo vertShaderStageInfo{.stage = vk::ShaderStageFlagBits::eVertex, .module = shaderModule, .pName = "vertMain"};
+            vk::PipelineShaderStageCreateInfo fragShaderStageInfo{.stage = vk::ShaderStageFlagBits::eFragment, .module = shaderModule, .pName = "fragMain"};
+            vk::PipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
         }
+
+        [[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char> &code) const {
+            vk::ShaderModuleCreateInfo createInfo{.codeSize = code.size() * sizeof(char), .pCode = reinterpret_cast<const uint32_t *>(code.data())};
+            vk::raii::ShaderModule     shaderModule{device, createInfo};
+
+            return shaderModule;
+	    }
+
+        static std::vector<char> readFile(const std::string &filename) {
+            std::ifstream file(filename, std::ios::ate | std::ios::binary);
+            if (!file.is_open()) {
+                throw std::runtime_error("failed to open file!");
+            }
+            std::vector<char> buffer(file.tellg());
+            file.seekg(0, std::ios::beg);
+            file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+            file.close();
+            return buffer;
+	    }
 };
 
 int main() {
