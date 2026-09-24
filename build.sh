e5cb8e099077b2765c7b6c81d765f80531f10de9
Ritesh Patel <riteshzeronity@gmail.com>
2026-09-24 23:44:07 +0530

    add shader compilation in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+$VULKAN_SDK/bin/slangc shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o shaders/slang.spv && \
 cmake -B build && \
 cmake --build build && \
 ./build/bin/BeLightVulkan
